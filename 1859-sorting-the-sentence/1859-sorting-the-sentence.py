c8f73f325efcacefe61e75abc9250cf37a05b0a4
nathy-min <69003813+nathy-min@users.noreply.github.com>
2022-03-01 02:42:48 -0800

    Time: 24 ms (52.67%), Space: 13.4 MB (74.06%) - LeetHub

--- 1859-sorting-the-sentence/1859-sorting-the-sentence.py
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.py
@@ -1,12 +1,16 @@
-class Solution:
-    def sortSentence(self, s: str) -> str:
-             l = s.split(' ')
+class Solution(object):
+    def sortSentence(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        rtype=[]
+        d={}
+        a=s.split(' ')
+        for l in a:
+            d[l[-1]]=l[:-1]
+        for i in range(1,len(d)+1):
+            b=str(i)
+            rtype.append(d[b])
         
-             ll = []
-        
-             for w in l:
-                ll.append([w[-1], w[:-1]])
-        
-             ll.sort(key = lambda x: x[0])
-        
-             return ' '.join([word[1] for word in ll])
+        return(' '.join(rtype))
