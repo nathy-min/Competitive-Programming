ca8e9b276baf3ab53d0308c864cdbfbc6ca0b677
nathy-min <69003813+nathy-min@users.noreply.github.com>
2022-03-29 12:16:20 -0700

    Time: 655 ms (74.51%), Space: 37.6 MB (28.43%) - LeetHub

---
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def minSetSize(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: int
+        """
+        c= Counter(arr)
+        N=len(arr)
+        n=0
+        output=0
+        for k,v in sorted(c.items(),key=lambda x: -x[1]):
+            n+=v
+            output+=1
+            if n>= N//2: break
+                
+        
+        return output
