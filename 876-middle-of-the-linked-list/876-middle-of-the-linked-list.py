ce135b323a081e1797f74a5705b965217fa4f526
nathy-min <69003813+nathy-min@users.noreply.github.com>
2022-06-14 11:29:53 -0700

    Time: 29 ms (31.22%), Space: 13.4 MB (78.14%) - LeetHub

--- 876-middle-of-the-linked-list/876-middle-of-the-linked-list.py
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.py
@@ -9,17 +9,8 @@ class Solution(object):
         :type head: ListNode
         :rtype: ListNode
         """
-        leng=0
-        start = node = head
-        while start:
-            leng +=1
-            start = start.next
-        middle = leng//2
-        counter=0
-        while node:
-            if counter == middle:
-                return node
-            else:
-                counter +=1
-                node = node.next
-        return None         
+        slow=fast= head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow= slow.next
+        return slow    
