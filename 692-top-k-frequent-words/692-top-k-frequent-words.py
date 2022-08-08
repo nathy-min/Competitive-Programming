555b2607cdea61c7373328ebfa09a7fed56d427e
nathy-min <69003813+nathy-min@users.noreply.github.com>
2022-08-08 13:48:15 -0700

    Time: 157 ms (5.46%), Space: 14.3 MB (5.33%) - LeetHub

--- 692-top-k-frequent-words/692-top-k-frequent-words.py
+++ 692-top-k-frequent-words/692-top-k-frequent-words.py
@@ -1,25 +1,20 @@
-class Solution:
-    def topKFrequent(self, words: List[str], k: int) -> List[str]:
-        class Word:
-            def __init__(self, freq, word):
-                self.word = word
-                self.freq = freq
-            def __lt__(self, other):
-                if self.freq == other.freq:
-                    return self.word > other.word 
-                return self.freq < other.freq
-        count = {}
-        # O(n)
-        for word in words:
-            count[word] = count.get(word, 0) + 1
-        heap = []
-        # O(nlogk)
-        for i in count:
-            heapq.heappush(heap, Word(count[i], i))
-            if len(heap) > k:
-                heappop(heap)
-        output = []
-        # O(n)
-        for i in range(len(heap)):
-            output.append(heappop(heap).word)
-        return output[::-1]
+class Solution(object):
+    def topKFrequent(self, words, k):
+        """
+        :type words: List[str]
+        :type k: int
+        :rtype: List[str]
+        """
+        c= Counter(words)
+        
+        c=[(-v,k) for k,v in c.items()]
+        heapq.heapify(c)
+
+        res=[]
+        i=0
+        while i<k and len(c)!=0:
+            res.append(heapq.heappop(c)[1])
+            print(c)
+            i+=1
+            
+        return res    
