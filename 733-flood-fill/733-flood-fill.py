6dd92cadbe700db8dc620df3dc7850bd9576f7fc
nathy-min <69003813+nathy-min@users.noreply.github.com>
2022-08-22 00:18:48 -0700

    Time: 108 ms (61.92%), Space: 14.3 MB (14.45%) - LeetHub

--- 733-flood-fill/733-flood-fill.py
+++ 733-flood-fill/733-flood-fill.py
@@ -1,18 +1,14 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
         rows,cols = len(image),len(image[0])
+        curC=image[sr][sc]
         def dfs(r,c):
-            nonlocal stpx
-            if r == rows or r < 0 or c == cols or c < 0 or image[r][c] != stpx or (r,c) in visited:
-                return
-            image[r][c] = color
-            visited.add((r,c))
-            dirn = [[1,0],[-1,0],[0,1],[0,-1]]
-            for i,j in dirn:
-                dfs(r +i,c + j)
-        visited = set()
-        
-        stpx = image[sr][sc]
-        dfs(sr,sc)
+            if 0<=r<rows and 0<=c<cols and image[r][c] == curC and image[r][c]!=color:
+                image[r][c] = color
+                dfs(r+1,c)
+                dfs(r-1,c)
+                dfs(r,c+1)
+                dfs(r,c-1)
+        dfs(sr,sc)        
         
         return image
