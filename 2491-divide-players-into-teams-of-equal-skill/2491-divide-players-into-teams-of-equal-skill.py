a3a58d49e9341cf09109374439d2aaa9f6c638e2
nathy-min <69003813+nathy-min@users.noreply.github.com>
2023-02-11 23:46:58 +0300

    Time: 583 ms (47.87%), Space: 27.9 MB (42.42%) - LeetHub

---
+++ 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.py
@@ -0,0 +1,25 @@
+class Solution:
+    def dividePlayers(self, skill: List[int]) -> int:
+        total_sum = sum(skill)              # Total sum
+        n = len(skill)
+        
+        total_no = n//2              # No of pairs
+                
+        desired = total_sum // total_no        # Desired sum of each pair
+                
+        skill.sort()
+        
+        chemistry_tot = 0
+        
+        l = 0                           # Two Pointer (Two Sum)             
+        r = n-1
+        
+        while l<r:
+            if skill[l]+skill[r]==desired:
+                chemistry_tot+=skill[l]*skill[r]
+            else:
+                return -1
+            l+=1
+            r-=1
+            
+        return chemistry_tot
